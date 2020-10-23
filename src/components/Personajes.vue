259288334f1828240807014dc3228322e952d05d
Your Name <you@example.com>
2020-10-23 00:05:23 -0300

    Primer commit

---
+++ src/components/Personajes.vue
@@ -0,0 +1,37 @@
+<template>
+  <main>
+    <InfoPersonajes :datosRyM="datosRyM"/>
+  </main>
+</template>
+
+<script>
+import InfoPersonajes from './mainComponents/InfoPersonaje.vue';
+
+export default {
+  name: 'Personajes',
+  components: {
+    InfoPersonajes
+  },
+
+  data() {
+    return {
+      datosRyM: [
+        {name: 'Rick Sanchez', image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'},
+        {name: 'Morty Smith', image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'},
+        {name: 'Summer Smith', image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'},
+        {name: 'Beth Smith', image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'},
+        {name: 'Jerry Smith', image: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg'},
+        {name: 'Abadango Cluster Princess', image: 'https://rickandmortyapi.com/api/character/avatar/6.jpeg'},
+      ]
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.imgFondo {
+  background-image: url('../assets/cropped-RM_page-header_background1-3.png');
+  background-size: cover;
+}
+</style>
---
+++ src/components/mainComponents/InfoPersonaje.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div v-for="(item, index) in datosRyM" :key="index">
+            <img :src="item.image" :alt="item.name"> <span>{{item.name}}</span>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'InfoPersonaje',
+    props: {
+        datosRyM: {
+            type:Array,
+            required: true
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
